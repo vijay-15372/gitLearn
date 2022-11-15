890c46c819b57fed7ef5c6303a2e6fc8ee13f847
vijay-15372 <gokulvpk8@gmail.com>
2022-11-15 20:01:46 +0530

    msg

--- src/main/java/gitLearn/class2.java
+++ src/main/java/gitLearn/class2.java
@@ -8,12 +8,7 @@ public class class2 {
 	public void method1() {
 		int x;
 		int y;
-<<<<<<< HEAD
-		this.x=10;
-		System.out.println("hi");
-		System.out.println("hello");
-=======
->>>>>>> parent of d8086cf... Revert "Revert "revereting""
+
 		
 	}
 
